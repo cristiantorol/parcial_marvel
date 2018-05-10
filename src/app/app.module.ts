3c17920ca9c1418a745d8cd510e972dd5d853418
Cristian <cristhian.toro@upb.edu.co>
2018-05-10 09:50:22 -0500

    filtro para buscar títulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule, Http } from '@angular/http';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ComicComponent } from './comic/comic.component';
 import { HeaderComponent } from './header/header.component';
+import { FilterPipe } from './filter.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ComicComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpModule
   ],
   providers: [],
